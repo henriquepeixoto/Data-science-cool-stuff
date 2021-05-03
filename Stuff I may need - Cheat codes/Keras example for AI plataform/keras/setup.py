3d9c4488728bb2567d344863c67a01cbf775a3e2
Henrique Peixoto Machado <55069048+henriquepeixoto@users.noreply.github.com>
2021-05-03 12:58:31 -0300

    Update setup.py

--- Stuff I may need - Cheat codes/Keras example for AI plataform/keras/setup.py
+++ Stuff I may need - Cheat codes/Keras example for AI plataform/keras/setup.py
@@ -2,7 +2,10 @@ from setuptools import find_packages
 from setuptools import setup
 
 REQUIRED_PACKAGES = ['Keras==2.0.4',
-                     'h5py==2.7.0']
+                     'h5py==2.7.0',
+                     'tensorflow>=2.4.1',
+                     'xgboost>=1.4.1'
+                     ]
 
 setup(
     name='trainer',
